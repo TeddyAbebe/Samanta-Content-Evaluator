155d930c3aca776e2c17c888cdc92a7b9e8656de
Teddy Abebe <teddyab729@gmail.com>
2023-07-21 15:31:10 -1100

    Update

--- src/ApiTester.js
+++ src/ApiTester.js
@@ -45,44 +45,46 @@ function ApiTester() {
   };
 
   return (
-    <div className="flex gap-10 pl-20 items-center">
-      <div className="flex flex-col w-[35%] bg-slate-900 justify-center items-center rounded-2xl">
-        <div className="flex flex-col w-full px-8 pt-16 pb-10">
-          <textarea
-            type="text"
-            value={inputValue}
-            onChange={handleInputChange}
-            placeholder="Content"
-            className=" rounded p-1 mb-3 h-56 w-full max-w-screen"
-          />
+    <div className="flex flex-col gap-5 items-center">
+      <div className="flex w-full justify-center items-center ">
+        <div className="flex flex-col w-[40%] bg-slate-900 justify-center items-center rounded-2xl">
+          <div className="flex flex-col w-full p-3">
+            <textarea
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              placeholder="Content"
+              className=" rounded p-1 mb-3 h-40 w-full max-w-screen"
+            />
 
-          <input
-            type="text"
-            value={title}
-            onChange={handleTitleChange}
-            placeholder="Title"
-            className="rounded p-1 w-1/2"
-          />
-        </div>
+            <input
+              type="text"
+              value={title}
+              onChange={handleTitleChange}
+              placeholder="Title"
+              className="rounded p-1 w-1/2"
+            />
+          </div>
 
-        <div className="flex justify-center gap-2 w-full p-2">
-          <button
-            className="w-[30%] hover:text-white font-medium font-serif rounded border-2 border-black bg-green-400 p-2"
-            onClick={handleApiRequest}
-          >
-            Check
-          </button>
+          <div className="flex justify-center gap-2 w-full p-2">
+            <button
+              className="w-[30%] hover:text-white font-medium font-serif rounded border-2 border-black bg-green-400 p-2"
+              onClick={handleApiRequest}
+            >
+              Check
+            </button>
 
-          <button
-            className="w-[30%] hover:text-white font-medium font-serif rounded border-2 border-black bg-red-800 p-2"
-            onClick={handleReset}
-          >
-            Reset
-          </button>
+            <button
+              className="w-[30%] hover:text-white font-medium font-serif rounded border-2 border-black bg-red-800 p-2"
+              onClick={handleReset}
+            >
+              Reset
+            </button>
+          </div>
         </div>
       </div>
 
-      <div className="text-white border-black bg-slate-900 justify-center items-center rounded-2xl max-w-2xl overflow-auto">
+      <div className="text-white border-black bg-slate-900 justify-center items-center rounded-2xl max-w-6xl overflow-hidden">
         {isLoading ? (
           <p className="p-4">Checking...</p>
         ) : response ? (
--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className=" bg-cyan-900 min-h-screen">
-      <div className="flex justify-center font-serif text-5xl py-24 italic font-semibold hover:text-white">
+    <div className=" bg-[#00fa9a] min-h-screen">
+      <div className="flex justify-center items-center text-center font-serif text-4xl w-full pt-5 pb-8 italic font-semibold hover:underline hover:decoration-dashed hover:underline-offset-8 hover:decoration-4">
         Samanta Content Evaluator
       </div>
       <ApiTester />
