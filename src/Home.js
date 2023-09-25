e071c025251d355138bd3e71541e3eaea7d36aa6
Tewodros Abebe <teddyab729@gmail.com>
2023-09-25 12:13:27 +0300

    Update

--- src/Home.js
+++ src/Home.js
@@ -261,7 +261,7 @@ function Home() {
                   onChange={handleInputChange}
                   rows="5"
                   placeholder="Content"
-                  className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 outline-none focus:ring-2 focus:ring-teal-300"
+                  className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 outline-none focus:ring-2 focus:ring-teal-300 resize-none"
                 ></textarea>
               </div>
 
